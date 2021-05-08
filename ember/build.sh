09832ca113bb316cef14958a622273a7d23d94e6
Ray Speth <speth@mit.edu>
2021-05-08 12:17:09 -0400

    First attempt to adapt Cantera's conda recipes

---
+++ ember/build.sh
@@ -0,0 +1,34 @@
+set +x
+
+echo "****************************"
+echo "BUILD STARTED"
+echo "****************************"
+
+if [[ "$DIRTY" != "1" ]]; then
+    scons clean
+fi
+
+rm -f ember.conf
+
+cp "${RECIPE_DIR}/../.ci_support/ember_base.conf" ember.conf
+
+echo "include = '${PREFIX}/include'" >> ember.conf
+echo "libdirs = '${PREFIX}/lib'" >> ember.conf
+
+if [[ "${OSX_ARCH}" == "" ]]; then
+    echo "CXX = '${CXX}'" >> ember.conf
+    echo "blas_lapack = 'mkl_rt,dl'" >> ember.conf
+else
+    echo "CXX = '${CLANGXX}'" >> ember.conf
+    echo "cxx_flags = '-isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}'" >> ember.conf
+fi
+
+set -xe
+
+scons build -j${CPU_COUNT}
+
+set +xe
+
+echo "****************************"
+echo "BUILD COMPLETED SUCCESSFULLY"
+echo "****************************"
